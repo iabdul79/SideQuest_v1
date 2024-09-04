2a4377010c4ba725eab0cb7ce247b9f69a31c2f4
iabdul79 <mabdulwadood79@gmail.com>
2024-09-05 00:27:43 +0530

    initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppHeader />
+      <NavigationDrawer />
+      <router-view />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import AppHeader from '@/components/AppHeader.vue';
+
+  //
+</script>
